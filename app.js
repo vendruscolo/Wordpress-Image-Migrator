a7c3f62d3bdf8ec23ca219dd960f40cf8788989d
Alessandro Vendruscolo <alessandro.vendruscolo@gmail.com>
2012-12-18 23:20:57 +0100

    Manage *all* posts

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ var rackspaceClient = cloudfiles.createClient({
 });
 
 // some DB queries
-var allPostsQuery = 'SELECT `ID`, `post_content` FROM `wp_posts` LIMIT 1';
+var allPostsQuery = 'SELECT `ID`, `post_content` FROM `wp_posts`';
 var updatePostContentQuery = 'UPDATE `wp_posts` SET `post_content` = :postContent WHERE ID = :postID';
 var rResource = /(http:\/\/(www\.)?macstories.net)?\/(stuff|wp-content\/uploads)\/.+?\.(jpe?g|gif|png|zip|rar|gz)/g;
 var rackspaceBucketName = config.rackspace.bucketName;
